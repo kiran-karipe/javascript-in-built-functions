9e950caf3708932a797c358ffa1d83b28754c4cc
Kiran <kiran.karipe@gmail.com>
2022-02-10 12:35:35 -0800

    updating debounce function

--- debounce.js
+++ debounce.js
@@ -1,17 +1,27 @@
 // Debouncing and throttling techniques are used to limit the number of times a function can execute
 
+const inputField = document.getElementById('input');
+inputField.addEventListener('keypress',  myDebounce(greet, 300))
+
 function myDebounce(func, waitTime) {
     let timer;
-    return () => {
+    return function() {
+        const args = arguments; // arguments is javascript keyword of type array like object
+        const context = this;
         clearTimeout(timer);
-        timer = setTimeout(func, waitTime);
+        timer = setTimeout(() => {
+            func.apply(context, args); // apply is used to pass arguments as an array,
+        }, waitTime); //context refers to 'this', we can pass null incase you don't need 'this'
     }
 }
 
-function myFunc() {
-    console.log('this is my event');
+function greet(name, title, age) {
+    // console.log(this.value); // call from inputField
+    console.log(name + ', ' + title + ', ' + age);
 }
 
-const onClickEvent = myDebounce(myFunc, waitTime);
-
-<button onclick="onClickEvent()">Click Here</button>
+/* const debouncedGreet = myDebounce(greet, 5000);
+debouncedGreet('kiran', 'karipe', '29');
+debouncedGreet('kiran', 'karipe', '29');
+debouncedGreet('kiran', 'karipe', '29');
+debouncedGreet('kiran', 'karipe', '29'); */
